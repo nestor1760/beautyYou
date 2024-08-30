fd1ee9f1acf316d7b995ff3c758f18b069c92ebd
Roman Nesterchuk <romannesterchuk01@gmail.com>
2024-08-30 20:26:50 +0200

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,7 +7,7 @@ import { useNavMenu } from '../hooks/useNavigation'
 
 const Header = () => {
   const { scrollToStart } = useNavMenu();
-
+  
   return (
     <header className='w-full max-w-[1110px] mx-auto flex items-center justify-between fixed top-0 fontRasa text-whiteColor z-10'>
       <Logo fontSize='default' spanSize='default' onClick={() => scrollToStart()} />
