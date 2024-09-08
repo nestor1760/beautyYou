50ae8f5cbb20a0297009b72b12950f908816af58
Nestor1760 <romannesterchuk01@gmail.com>
2024-09-08 21:46:05 +0200

    last updates

--- app/pages/GalleryPart.tsx
+++ app/pages/GalleryPart.tsx
@@ -9,7 +9,7 @@ const GalleryPart = () => {
   return (
     <section id='gallery' className='flex flex-col items-start justify-between w-full max-w-[1110px] mx-auto'>
       <NavigationItem label='gallery' />
-      <div className='mt-[65px] mb-[120px] flex items-center justify-start flex-wrap gap-[30px]'>
+      <div className='mt-[65px] mb-[120px] grid grid-rows-2 grid-cols-5 gap-[30px]'>
         {galleryData.map(photo =>
           <Image
             src={photo.path}
